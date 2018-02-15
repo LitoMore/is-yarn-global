b73c21d7239c63dbed5d5ee1ce2c2905ab571817
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-02-15 11:33:56 +0700

    Use __dirname instead of process.cwd()

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = function () {
 	const isWindows = process.platform === 'win32';
 	const yarnPath = isWindows ? path.join('Yarn', 'config', 'global') : path.join('.config', 'yarn', 'global');
-	if (process.cwd().includes(yarnPath)) {
+	if (__dirname.includes(yarnPath)) {
 		return true;
 	}
 	return false;
