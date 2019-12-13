dcb8775e149483b94cb72a20e77e68c1bacd3c21
shukebeta <weizhong2004@gmail.com>
2019-12-14 01:37:31 +1300

    Code optimization (#3)

--- index.js
+++ index.js
@@ -5,8 +5,5 @@ const path = require('path');
 module.exports = function () {
 	const isWindows = process.platform === 'win32';
 	const yarnPath = isWindows ? path.join('Yarn', 'config', 'global') : path.join('.config', 'yarn', 'global');
-	if (__dirname.includes(yarnPath)) {
-		return true;
-	}
-	return false;
+	return __dirname.includes(yarnPath);
 };
